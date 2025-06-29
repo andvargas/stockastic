a4deba38930178d57bb5a946018681e488c6842c
Andras <a.vargyas@gmail.com>
2025-06-29 16:26:56 +0300

    Tooltip fixed

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 const Tooltip = ({ children, tooltipText, position = "top", showOnLoadDuration = 0 }) => {
   const [showOnLoad, setShowOnLoad] = useState(false);
 
-  // Map positions to Tailwind classes
   const positionClasses = {
     top: "bottom-full mb-1 left-1/2 transform -translate-x-1/2",
     bottom: "top-full mt-1 left-1/2 transform -translate-x-1/2",
@@ -12,12 +11,14 @@ const Tooltip = ({ children, tooltipText, position = "top", showOnLoadDuration =
   };
 
   useEffect(() => {
-    if (showOnLoadDuration > 0) {
+    if (typeof showOnLoadDuration === "number" && showOnLoadDuration > 0) {
       setShowOnLoad(true);
       const timer = setTimeout(() => {
         setShowOnLoad(false);
       }, showOnLoadDuration);
       return () => clearTimeout(timer);
+    } else {
+      setShowOnLoad(false);
     }
   }, [showOnLoadDuration]);
 
@@ -25,11 +26,11 @@ const Tooltip = ({ children, tooltipText, position = "top", showOnLoadDuration =
     <div className="relative group inline-block">
       {children}
       <div
-        className={`absolute 
+        className={`absolute
         ${showOnLoad || "group-hover:opacity-100 group-hover:scale-100"}
         opacity-${showOnLoad ? "100" : "0"}
-        transition-opacity transition-transform duration-200 ease-out 
-        bg-neutral-500 text-white text-xs rounded py-1 px-2 whitespace-nowrap z-10 pointer-events-none scale-${showOnLoad ? "100" : "95"} 
+        transition-opacity transition-transform duration-200 ease-out
+        bg-neutral-500 text-white text-xs rounded py-1 px-2 whitespace-nowrap z-10 pointer-events-none scale-${showOnLoad ? "100" : "95"}
         ${positionClasses[position]}`}
       >
         {tooltipText}
